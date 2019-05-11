93c1e95ac4300b7251da7b4d2b6390bbd2e4af3e
Kristian <kriskobota@abv.bg>
2019-05-11 23:15:20 +0300

    Create "calculateBudget" function, add some validations

--- app.js
+++ app.js
@@ -14,6 +14,18 @@ let budgetController = (function () {
         this.value = value;
     };
 
+    //Calculate the total sum of income or expenses
+    let calculateTotal = function (type) {
+
+        let sum = 0;
+
+        data.allItems[type].forEach(function (current) {
+            sum += current.value
+        });
+
+        data.total[type] = sum;
+    };
+
     let data = {
         allItems: {
             exp: [],
@@ -23,24 +35,26 @@ let budgetController = (function () {
         total: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
 
-    return{
+    return {
         addItem: function (type, description, value) {
             let item, ID;
 
             // Create ID
-            if(data.allItems[type].length > 0){
+            if (data.allItems[type].length > 0) {
                 ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
-            }else{
+            } else {
                 ID = 0;
             }
 
             //Create new item
-            if(type === 'exp'){
+            if (type === 'exp') {
                 item = new Expense(ID, description, value);
-            }else if(type === 'inc'){
+            } else if (type === 'inc') {
                 item = new Income(ID, description, value);
             }
 
@@ -48,6 +62,30 @@ let budgetController = (function () {
             data.allItems[type].push(item);
 
             return item;
+        },
+
+        calculateBudget: function () {
+            //Calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+            //Calculate the budget: income - expenses
+            data.budget = data.total.inc - data.total.exp;
+
+            if(data.total.inc > 0){
+                // Calculate the percentage of income that we spent
+                data.percentage = Math.round((data.total.exp / data.total.inc) * 100)
+            }else{
+                data.percentage = -1
+            }
+        },
+
+        returnBudget: function () {
+            return {
+                budget: data.budget,
+                totalInc: data.total.inc,
+                totalExp: data.total.exp,
+                percentage: data.percentage
+            };
         },
         //Testing function
         testing: function () {
@@ -74,7 +112,7 @@ let UIController = (function () {
             return {
                 type: document.querySelector(DOMStrings.sign).value,
                 description: document.querySelector(DOMStrings.description).value,
-                value: document.querySelector(DOMStrings.value).value
+                value: parseFloat(document.querySelector(DOMStrings.value).value)
             }
         },
         getDOM: function () {
@@ -84,13 +122,13 @@ let UIController = (function () {
             let html, newHtml, element;
 
             //Create HTML string with placeholder text
-            if(type === 'exp'){
+            if (type === 'exp') {
                 element = DOMStrings.expensesList;
 
                 html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div>' +
                     '<div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete">' +
                     '<button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
-            }else if(type === 'inc'){
+            } else if (type === 'inc') {
                 element = DOMStrings.incomeList;
 
                 html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div>' +
@@ -126,7 +164,7 @@ let UIController = (function () {
 //GLOBAL APP CONTROLLER
 let controller = (function (budgetCtrl, UICtrl) {
 
-    let setupEventListeners = function(){
+    let setupEventListeners = function () {
         let DOMStrings = UIController.getDOM();
         document.querySelector(DOMStrings.addButton).addEventListener('click', ctrlAddItem);
 
@@ -137,26 +175,37 @@ let controller = (function (budgetCtrl, UICtrl) {
         });
     };
 
+    function calculateBudget() {
+        //1. Calculate the budget
+        budgetController.calculateBudget();
+        //2. return the budget
+        let budget = budgetController.returnBudget();
+
+        console.log(budget);
+        //3. Display the budget on the UI
+    }
+
     let ctrlAddItem = function () {
         //1. Get the field input data
         let input = UIController.getInput();
 
-        //2. Add the item to the budget controller
-        let newItem = budgetController.addItem(input.type, input.description, input.value);
-        budgetController.testing();
+        //Validate data
+        if (input.description !== "" && !isNaN(input.value) && input.value !== 0) {
+            //2. Add the item to the budget controller
+            let newItem = budgetController.addItem(input.type, input.description, input.value);
 
-        //3. Add the item to the UI
-        UIController.addListItem(newItem, input.type);
+            //3. Add the item to the UI
+            UIController.addListItem(newItem, input.type);
 
-        //4. Clear fields value
-        UIController.clearFields();
+            //4. Clear fields value
+            UIController.clearFields();
 
-        //4. Calculate the budget
-
-        //5. Display the budget on the UI
+            //5. Calculate and update budget
+            calculateBudget();
+        }
     };
 
-    return{
+    return {
         init: function () {
             setupEventListeners();
         }
